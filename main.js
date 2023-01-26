5aa2be7f05873a9724fee629c85d37da86b812c1
Ulee Mammadzada <ulee@Ulees-MacBook-Air.local>
2023-01-26 18:40:50 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let Palindrome = prompt ("Enter something");
+let result = Palindrome.split("").reverse("").join("")
+
+
+const myFunction = () => {
+    if (Palindrome == result) {
+        console.log ("true");
+    }else{
+        console.log ("false");
+    }
+}
+
+myFunction();
